2ff7b0aba83d5b22ce15b5bff166019caa0d83ce
Mohamed-Alfayas <fayas5414@gmail.com>
2024-12-12 11:52:20 +0530

    Resume Drive link Change

--- src/data/constants.js
+++ src/data/constants.js
@@ -14,7 +14,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Mohamed-Alfayas",
   resume:
-    "https://drive.google.com/file/d/1-JKtkNr-lKaOHrucrKPrCTR7swZDbQTW/view",
+    "https://drive.google.com/file/d/1WkpkhUuT_w7PuFsB1Lkkde69w2JCH4pJ/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/mohamed-alfayas-5a2487227/",
   
 };
